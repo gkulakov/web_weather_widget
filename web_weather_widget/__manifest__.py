23ab16804a61698dc9d8096bffbfd22d6f1b1d9c
George <egor.sxe@gmail.com>
2023-02-11 22:47:05 +0300

    [INIT]

---
+++ web_weather_widget/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "Navbar Weather Widget",
+
+    "summary": """
+        Add Weather Widget for Odoo navbar
+    """,
+
+    "description": """
+        Add Weather Widget for Odoo navbar
+    """,
+
+    "author": "Georgy Kulakov",
+    "website": "https://icode.by",
+
+    "category": "Customizations",
+    "version": "0.1",
+
+    "depends": ["base"],
+    
+    "assets": {
+        "web.assets_backend": [
+            "web_weather_widget/static/src/components/*/*",
+        ],
+    }
+}
---
+++ web_weather_widget/static/src/components/weather_widget/weather_widget.js
@@ -0,0 +1,91 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+
+const { Component, useState, onMounted, onWillStart, OwlError, EventBus } = owl;
+
+const systrayRegistry = registry.category("systray");
+
+class WeatherWidget extends Component {
+    setup() {
+        super.setup();
+        this.state = useState({
+            current_temp: 0,
+            weather_icon: "ban",
+        });
+        this.latitude = 0;
+        this.longitude = 0;
+        this.env.bus.addEventListener("update_widget_states", ({detail}) => {
+            this.updateWeatherInformation(detail.latitude, detail.longitude);
+        });
+        onWillStart(async () => {
+            this.getCoordinates();
+            setInterval(async () => {
+                this.getWeatherInfo();
+            }, 5*60*1000);
+        })
+    }
+
+    /**
+     * Match weather icon from weather code
+     */
+    getWeatherIcon(code) {
+        let weather_code = {icon: "thermometer-half", description: "Default"}
+        if ([1, 2, 3].includes(code)) {
+            weather_code = {icon: "sun-o", description: "Clear sky, mainly clear, partly cloudy, and overcast"};
+        } else if ([51, 53, 55, 61, 63, 65, 66, 67, 80, 81, 82].includes(code)) {
+            weather_code = {icon: "umbrella", description: "Rain"};
+        } else if ([56, 57, 71, 73, 75, 77, 85, 86].includes(code)) {
+            weather_code = {icon: "snowflake-o", description: "Snow"};
+        } else if ([95, 96, 99].includes(code)) {
+            weather_code = {icon: "bolt", description: "Thunderstorm"};
+        }
+        return weather_code.icon;
+    }
+
+    /**
+     * Run `update_widget_states` trigger
+     */
+    getWeatherInfo() {
+        this.env.bus.trigger("update_widget_states", {
+            latitude: this.latitude,
+            longitude: this.longitude,
+        });
+    }
+
+    /**
+     * Requesting geolocation from a browser
+     * Save coordinates if success and call getWeatherInfo()
+     */
+    getCoordinates() {
+        const self = this;
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                    const { latitude, longitude } = position.coords;
+                    self.latitude = latitude;
+                    self.longitude = longitude;
+                    self.getWeatherInfo();
+                }
+            )
+        }
+    }
+    
+    async updateWeatherInformation(latitude, longitude) {
+        const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=1`
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new OwlError("Cannot get weather information from weather API");
+        }
+        const resp = await response.json()
+        this.state.current_temp = resp["current_weather"]["temperature"];
+        this.state.weather_icon = this.getWeatherIcon(resp["current_weather"]["weathercode"])
+    }
+}
+
+WeatherWidget.template = "web_weather_widget.WeatherWidget";
+
+systrayRegistry.add(
+    "web_weather_widget.WeatherWidget",
+    {Component: WeatherWidget},
+    { sequence: 0 },
+    );
